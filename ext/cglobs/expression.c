2f36b1f4284e64e347e34bb14597771b1ffd3553
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2019-01-14 12:13:13 +0100

    improve naming variables

--- ext/cglobs/expression.c
+++ ext/cglobs/expression.c
@@ -12,46 +12,46 @@ int get_group(const char *source, VALUE *ruby_array) {
 
 int get_matches(const char *source, const char *regexString,
                 VALUE *ruby_array) {
-  unsigned int g, m, offset;
+  unsigned int group_index, match_index = 0;
 
-  regex_t regexCompiled;
-  regmatch_t groupArray[MAX_GROUPS];
+  long long offset = 0;
+
+  regex_t compiled_regex;
+  regmatch_t groups_array[MAX_GROUPS];
   char *cursor;
 
-  if (regcomp(&regexCompiled, regexString, REG_EXTENDED)) {
+  // compile REGEX
+  if (regcomp(&compiled_regex, regexString, REG_EXTENDED)) {
     printf("Could not compile regular expression.\n");
     printf("Could not compile regular expression.\n");
     return EXIT_FAILURE;
   };
 
-  m = 0;
   cursor = source;
-  for (m = 0; m < MAX_MATCHES; m++) {
-    if (regexec(&regexCompiled, cursor, MAX_GROUPS, groupArray, 0)) {
+  for (match_index = 0; match_index < MAX_MATCHES; match_index++) {
+    if (regexec(&compiled_regex, cursor, MAX_GROUPS, groups_array, 0)) {
       printf("No more matches.\n");
       break; // No more matches
     }
 
-    m = g = offset = 0;
-
-    for (g = 0; g < MAX_GROUPS; g++) {
-      regmatch_t group_regmatch = groupArray[g];
+    for (group_index = 0; group_index < MAX_GROUPS; group_index++) {
+      regmatch_t group_regmatch = groups_array[group_index];
 
       if (group_regmatch.rm_so == (size_t)-1)
         break; // No more groups
 
-      if (g == 0) {
+      if (group_index == 0) {
         offset = group_regmatch.rm_eo;
       }
 
-      char cursorCopy[strlen(cursor) + 1];
-      strcpy(cursorCopy, cursor);
-      cursorCopy[group_regmatch.rm_eo] = 0;
+      char cursor_copy[strlen(cursor) + 1];
+      strcpy(cursor_copy, cursor);
+      cursor_copy[group_regmatch.rm_eo] = 0;
 
-      char *matched_string = cursorCopy + group_regmatch.rm_so;
+      char *matched_string = cursor_copy + group_regmatch.rm_so;
 
-      printf("Match %u, Group %u: [%2u-%2u]: %s\n", m, g, group_regmatch.rm_so,
-             group_regmatch.rm_eo, matched_string);
+      printf("Match %u, Group %u: [%2u-%2u]: %s\n", match_index, group_index,
+             group_regmatch.rm_so, group_regmatch.rm_eo, matched_string);
 
       VALUE str_value_str = rb_str_new2(matched_string);
       rb_ary_push(ruby_array, str_value_str);
@@ -59,7 +59,7 @@ int get_matches(const char *source, const char *regexString,
     cursor += offset;
   }
 
-  regfree(&regexCompiled);
+  regfree(&compiled_regex);
 
   return EXIT_SUCCESS;
 }
