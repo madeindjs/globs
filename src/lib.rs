23b6b03737951955b63d4e41f69b8e54399308e9
Alexandre Rousseau <contact@rousseau-alexandre.Fr>
2019-01-15 11:57:14 +0100

    Find group using REGEX

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+extern crate regex;
+use regex::Regex;
+
 /// Expand the given string and return all possibles strings
 pub fn expands(expandable: &str) -> Vec<String> {
     let mut expands: Vec<String> = Vec::new();
+
+    let re: Regex = Regex::new(r"\{(([A-z]+,?)+)\}").unwrap();
+
+    println!(
+        "Expands '{}' match ?: {}",
+        expandable,
+        re.is_match(expandable)
+    );
+
+    for cap in re.captures_iter(expandable) {
+        println!("Month: {}", &cap[0]);
+    }
 
     expands.push(expandable.to_string());
 
@@ -10,7 +25,15 @@ pub fn expands(expandable: &str) -> Vec<String> {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn no_matches() {
+    fn without_any_group() {
         assert_eq!(vec!["Hello"], super::expands("Hello"));
+    }
+
+    #[test]
+    fn no_matches() {
+        let input = "https://{qwant,duckduckgo}.com/";
+        let expected = vec!["https://qwant.com/", "https://duckduckgo.com/"];
+
+        assert_eq!(expected, super::expands(input));
     }
 }
